7fb72121630277fb3ddef908126e49924d6df7b9
Tekyto <Tekyto2014@gmail.com>
2022-10-19 00:01:01 +0300

    queen

--- index.cpp
+++ index.cpp
@@ -3,5 +3,5 @@
 #include <cmath>
 
 bool queen(int x1, int y1, int x2, int y2) {
-	return abs( x2 - x1) == abs (y2 - y1) || x1 == x2 || y1 == y2;
+	return abs(x2 - x1) == abs(y2 - y1) || x1 == x2 || y1 == y2;
 }
